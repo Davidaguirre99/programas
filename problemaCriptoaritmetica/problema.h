ab3a0350d7bade0db4776f16739ceacb08f43214
David Aguirre <david.aguirre@epn.edu.ec>
2022-06-25 00:25:32 -0500

    cirptoaritmetica  problema solucion intento

---
+++ problemaCriptoaritmetica/problema.h
@@ -0,0 +1,21 @@
+#ifndef PROBLEMA_H_INCLUDED
+#define PROBLEMA_H_INCLUDED
+#include<iostream>
+using namespace std;
+class Estado{
+public:
+  int T,W,O,F,U,R,c1,c2,c3;
+  Estado(){
+    T=0;
+    W=0;
+    O=0;
+    F=0;
+    U=0;
+    R=0;
+    c1=0;
+    c2=0;
+    c3=0;
+  }
+};
+
+#endif // PROBLEMA_H_INCLUDED
